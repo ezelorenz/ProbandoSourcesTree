19744b7764bc09449c2d3687f7b68dd0386f40cb
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-08-29 15:37:28 -0300

    Creacion de repositorio generico

---
+++ ApiBibliotecaSourcestree/ApiBibliotecaSourcestree/DAL/Implementaciones/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiBibliotecaSourcestree.DAL.Implementaciones
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Actualizar(T entity)
+        {
+            bool resultado = false;
+
+            _context.Set<T>().Update(entity);
+            resultado = await _context.SaveChangesAsync() > 0;
+            return resultado;
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            var entidad = await Obtener(id);
+            if (entidad == null)
+            {
+                return false;
+            }
+            _context.Set<T>().Remove(entidad);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Insertar(T entity)
+        {
+            bool resultado = false;
+            _context.Set<T>().AddAsync(entity);
+            resultado = await _context.SaveChangesAsync() > 0;
+            return resultado;
+        }
+
+        public async Task<T> Obtener(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+
+        public async Task<IEnumerable<T>> ObtenerTodos()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+    }
+}
---
+++ ApiBibliotecaSourcestree/ApiBibliotecaSourcestree/DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiBibliotecaSourcestree.DAL.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> ObtenerTodos();
+        Task<T> Obtener(int id);
+        Task<bool> Insertar(T entity);
+        Task<bool> Actualizar(T entity);
+        Task<bool> Eliminar(int id);
+    }
+}
