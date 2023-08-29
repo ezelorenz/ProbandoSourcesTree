8569f11691f1fd68dc82ddfd370c1113bf528895
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-08-29 15:31:10 -0300

    Creacion de controlador de libro

---
+++ ApiBibliotecaSourcestree/ApiBibliotecaSourcestree/Controllers/LibroController.cs
@@ -0,0 +1,26 @@
+﻿using BibliotecaBitwise.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBibliotecaSourcestree.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public LibroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Libro>>> ObtenerTodos()
+        {
+            var libros = await _context.Libros.ToListAsync();
+            return Ok(libros);
+        }
+    }
+}
