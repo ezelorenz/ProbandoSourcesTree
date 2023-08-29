893e1a8ee0d11a402e2cebd95ca13ec316f712cc
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-08-29 15:23:26 -0300

    Configuracion de entidades para crear migracion a Db

--- ApiBibliotecaSourcestree/ApiBibliotecaSourcestree/ApplicationDbContext.cs
+++ ApiBibliotecaSourcestree/ApiBibliotecaSourcestree/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BibliotecaBitwise.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiBibliotecaSourcestree
 {
@@ -8,5 +9,25 @@ namespace ApiBibliotecaSourcestree
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Genero>().Property(x => x.Nombre).HasMaxLength(150);
+
+
+            modelBuilder.Entity<Autor>().Property(x => x.Nombre).HasMaxLength(150);
+            modelBuilder.Entity<Autor>().Property(x => x.FechaNacimiento).HasColumnType("date");
+
+            modelBuilder.Entity<Libro>().Property(x => x.Titulo).HasMaxLength(200);
+            modelBuilder.Entity<Libro>().Property(x => x.FechaLanzamiento).HasColumnType("date");
+
+            modelBuilder.Entity<Comentario>().Property(x => x.Contendo).HasMaxLength(500);
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<Libro> Libros { get; set; }
     }
 }
